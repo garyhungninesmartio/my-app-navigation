2448252aabaa562d1dd1f8ad07cedbc7921d93dd
Gary Hung <gary.hung@ninesmart.io>
2023-09-04 18:07:57 +0800

    Create folder to hold related pages in home

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import HomeLandingPage from './pages/home-landing';
+import HomeLandingPage from './pages/home';
 
 const Stack = createNativeStackNavigator();
 
